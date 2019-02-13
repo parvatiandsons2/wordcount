185e08b4f776649e816343cad81f7a1498ce681a
NeerajPathak <npathak56@gmail.com>
2019-02-13 17:02:49 +0530

    First Commit v3

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.homepage, name='home'),
     path('counts/', views.count, name='count'),
     path('about/', views.about, name='about'),
 ]
